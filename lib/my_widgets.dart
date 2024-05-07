bae19936611e84eccc7201c164e6a09bc0e6bb6d
etoncel <ernesto.toncel@gmail.com>
2024-05-07 07:05:23 -0500

    Create new widget.

--- lib/my_widgets.dart
+++ lib/my_widgets.dart
@@ -1,7 +1,17 @@
 library my_widgets;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "My Widget",
+        style: TextStyle(fontSize: 40),
+      ),
+    );
+  }
 }
